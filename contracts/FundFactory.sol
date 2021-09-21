85e36d25ad8975f62c7160430c180739eb4342df
kostyamospan <37243242+kostyamospan@users.noreply.github.com>
2021-09-21 13:33:55 +0300

    removed after merge conflits

--- contracts/FundFactory.sol
+++ contracts/FundFactory.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma experimental ABIEncoderV2;
 pragma solidity ^0.6.6; // because of uni|cake swap
-pragma experimental ABIEncoderV2;
 
 import "./SharedImports.sol";
 import "./HedgeFund.sol";
--- contracts/Interfaces/IFundFactory.sol
+++ contracts/Interfaces/IFundFactory.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma experimental ABIEncoderV2;
 pragma solidity ^0.6.6;
-pragma experimental ABIEncoderV2;
 
 import "./IHedgeFund.sol";
 import "../Types/HedgeFundInfo.sol";
